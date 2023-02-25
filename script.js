fa844be2df151ddc1e83f0fdee7c080b348a2651
aeblinksup <126326592+aeblinksup@users.noreply.github.com>
2023-02-25 12:27:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+//Verknüpfungen zu den verschiedenen Privaten-Seiten
+function LW() {
+    location.replace("wulf.html")
+}
+
+function DB() {
+    location.replace("binotto.html")
+}
+
+function LG() {
+    location.replace("gröbli.html")
+}
+
+function DW() {
+    location.replace("wüest.html")
+}
+
+function JZ() {
+    location.replace("zwicker.html")
+}
+
+function SR() {
+    location.replace("ruf.html")
+}
+
+function SH() {
+    location.replace("hossli.html")
+}
+
+function KL() {
+    location.replace("klassenlehrer.html")
+}
+
+function JM() {
+    location.replace("moser.html")
+}
+//-----------------------------------------------------
+
+
+//Home Button
+function Home() {
+    location.replace("home.html")
+}
+//-----------------------------------------------------
+
+
+//Back Button (Nutzungsbedingungen)
+function Back() {
+    location.replace("index.html")
+}
+//-----------------------------------------------------
+
+
+//Accept button
+function Login() {
+    location.replace("home.html")
+}
+//-----------------------------------------------------
